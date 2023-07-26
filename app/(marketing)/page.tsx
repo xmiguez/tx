4308934378d92b10fb68e9214dab17a776b06dc2
xmiguez <xmiguez@gmail.com>
2023-07-26 20:32:51 +0200

    fix hero

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ export default async function IndexPage() {
       <section className=" pt-48">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <h1 className="font-heading text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-            My blog personal
+            Mi blog personal
           </h1>
           <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
             Todo lo que buscas en un solo lugar.
