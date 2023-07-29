b2c8f8541f238c4f3225530110ed77a5ba373bd5
xmiguez <xmiguez@gmail.com>
2023-07-29 15:33:02 +0200

    center hero

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
 export default async function IndexPage() {
   return (
     <>
-      <section className="pt-56 sm:pt-56">
+      <section className="  flex h-[80vh] items-center justify-center">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <h1 className="font-heading text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
             Mi blog personal
