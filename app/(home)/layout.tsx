892e90aad3b8784691ed09889ed41bc59b062609
xmiguez <xmiguez@gmail.com>
2023-08-04 18:55:19 +0200

    fix

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,16 +1,36 @@
-import { MainNav } from "@/components/main-nav"
-import { ModeToggle } from "@/components/mode-toggle"
+import Link from "next/link"
 
-export default async function MarketingLayout({ children }) {
+import { marketingConfig } from "@/config/marketing"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { MainNav } from "@/components/main-nav"
+
+interface MarketingLayoutProps {
+  children: React.ReactNode
+}
+
+export default async function MarketingLayout({
+  children,
+}: MarketingLayoutProps) {
   return (
-    <div className="flex flex-col">
-      <header className="z-40 bg-background px-7 lg:px-32">
+    <div className="flex min-h-screen flex-col">
+      <header className="container z-40 bg-background">
         <div className="flex h-20 items-center justify-between py-6">
-          <MainNav />
-          <ModeToggle />
+          <MainNav items={marketingConfig.mainNav} />
+          <nav>
+            <Link
+              href="/login"
+              className={cn(
+                buttonVariants({ variant: "secondary", size: "sm" }),
+                "px-4"
+              )}
+            >
+              Login
+            </Link>
+          </nav>
         </div>
       </header>
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
     </div>
   )
 }
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Bronx",
+  description: "",
+  url: "",
+  ogImage: "",
+  links: {
+    twitter: "",
+    github: "",
+  },
+}
